d81fcbc4df1ea99d43ca041495a7d797ef752efb
jungeun <jungeun0054@gmail.com>
2025-03-18 12:15:43 +0900

    Join, NotFound 출력

---
+++ semiprojectv2r/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+    // Main 함수 컴포넌트 정의
+    const Main = () => {
+        return (
+
+                <main id="content">
+                    <img src="image/day6.jpg" className="img-fluid"/>
+                </main>
+    )
+    }
+
+    export default Main;
