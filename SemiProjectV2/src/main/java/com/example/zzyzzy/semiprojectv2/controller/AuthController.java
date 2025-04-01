194fb29164ecdc0465a76461390ab08ab44cc466
zzyzzy <zzyzigy@gmail.com>
2025-04-01 12:19:50 +0900

    Jwt 인증 성공시 토큰을 응답으로 전달

--- SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/controller/AuthController.java
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/controller/AuthController.java
@@ -3,12 +3,20 @@ package com.example.zzyzzy.semiprojectv2.controller;
 import com.example.zzyzzy.semiprojectv2.domain.Member;
 import com.example.zzyzzy.semiprojectv2.domain.MemberDTO;
 import com.example.zzyzzy.semiprojectv2.domain.User;
+import com.example.zzyzzy.semiprojectv2.jwt.JwtTokenProvider;
 import com.example.zzyzzy.semiprojectv2.service.MemberService;
 import com.example.zzyzzy.semiprojectv2.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 // 교차출처 리소스 공유 CORS
 @CrossOrigin(origins="http://localhost:5173")
@@ -18,8 +26,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final MemberService memberService;
+    //private final MemberService memberService;
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<?> joinok(@RequestBody User user) {
@@ -48,14 +58,26 @@ public class AuthController {
         log.info("submit된 로그인 정보 : {}", user);
 
         try {
-            User loginUser = userService.loginUser(user);
-            // 세션 처리
-            response = ResponseEntity.ok().build();
-        } catch (IllegalStateException e) {
-            response = ResponseEntity.badRequest().body(e.getMessage());
-            e.printStackTrace();
+            // 스프링 시큐리티에서 제공하는 인증처리 매니저로 인증 처리
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserid(), user.getPasswd())
+            );
+
+            // 인증이 완료되면 jwt 토큰 생성
+            final String jwt = jwtTokenProvider.generateToken(user.getUserid());
+
+            // 생성한 토큰을 JSON 형식으로 만듦
+            Map<String, String> tokens = Map.of(
+                "accessToken", jwt
+            );
+
+            response = ResponseEntity.ok().body(tokens);
+        } catch (BadCredentialsException e) {
+            response = ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("아이디나 비밀번호가 일치하지 않습니다!!");
+            log.info(e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            log.info(e.getMessage());
         }
 
         return response;
