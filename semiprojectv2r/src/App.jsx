d10325bdaeda28a93cb327a71efa8b6e273b5a48
zzyzzy <zzyzigy@gmail.com>
2025-04-03 10:12:12 +0900

    AuthContext로 인증정보 저장 구현 인증여부에 따라 로그인/로그아웃 표시 로그아웃 기능 구현

--- semiprojectv2r/src/App.jsx
+++ semiprojectv2r/src/App.jsx
@@ -6,10 +6,11 @@ import Footer from "./pages/layout/Footer";
 import Main from "./pages/Main";
 import Join from "./pages/Join";
 import Login from "./pages/Login";
+import Logout from "./pages/Logout";
 import Myinfo from "./pages/Myinfo";
 import BoardList from "./pages/BoardList";
 import BoardWrite from "./pages/BoardWrite";
-
+import BoardView from "./pages/BoardView";
 
 import NotFound from "./pages/NotFound";
 import './styles/App.css'
@@ -24,6 +25,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/member/join" element={<Join />} />
         <Route path="/member/login" element={<Login />} />
+        <Route path="/member/logout" element={<Logout />} />
         <Route path="/board/list/:cpg" element={<BoardList />} />
         <Route path="/board/find/:ftype/:fkey/:cpg" element={<BoardList />} />
         <Route path="/board/write" element={<BoardWrite />} />
