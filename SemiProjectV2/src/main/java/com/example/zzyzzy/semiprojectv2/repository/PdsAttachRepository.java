e1eb9e161614116ed237105592b4508fe7996871
zzyzzy <zzyzigy@gmail.com>
2025-04-07 09:26:22 +0900

    자료실 관련 repository 구현

---
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/repository/PdsAttachRepository.java
@@ -0,0 +1,8 @@
+package com.example.zzyzzy.semiprojectv2.repository;
+
+import com.example.zzyzzy.semiprojectv2.domain.PdsAttach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsAttachRepository extends JpaRepository<PdsAttach, Long> {
+
+}
---
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/repository/PdsRepository.java
@@ -0,0 +1,8 @@
+package com.example.zzyzzy.semiprojectv2.repository;
+
+import com.example.zzyzzy.semiprojectv2.domain.Pds;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsRepository extends JpaRepository<Pds, Long> {
+
+}
